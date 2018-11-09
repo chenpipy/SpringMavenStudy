fa8f123eb1b85ec8c89d6ebc376a913370e0b003
chenjun <chenjun@toroot.cn>
2018-11-09 17:25:37 +0800

    增加log打印日志，但是无法查看（配置未配置好）

--- src/main/java/com/tmall/controller/StudentController.java
+++ src/main/java/com/tmall/controller/StudentController.java
@@ -1,6 +1,9 @@
 package com.tmall.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.tmall.service.impl.StudentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,6 +11,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 学习用的controller
@@ -20,12 +25,21 @@ public class StudentController {
 
     @Resource
     private StudentService studentService;
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @RequestMapping(value="/getAllStudent",method = RequestMethod.GET)
-    public ModelAndView getAllStudent(){
-        ModelAndView mav=new ModelAndView();
-        mav.setViewName("studentDisplay");
-        mav.addObject("students",studentService.getAllStudent());
-        return mav;
+    @RequestMapping(value = "/getAllStudent", method = RequestMethod.GET)
+    @ResponseBody
+    public String getAllStudent() {
+        Map<String, Object> map = new HashMap();
+        try {
+            map.put("code",1);
+            map.put("data", 1/0);
+        } catch (Exception e) {
+            map.put("code",0);
+            map.put("message",e.getMessage());
+            System.out.println();
+            logger.error(e.getMessage());
+        }
+        return JSON.toJSONString(map);
     }
 }
